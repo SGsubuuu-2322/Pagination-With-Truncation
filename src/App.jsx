fe1875bfc68cafe4db084b891d154eecc8090a4f
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-09-07 11:11:36 +0530

    Fetched products from API...

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function App() {
+  const fetchProducts = async () => {
+    const res = await fetch("https://dummyjson.com/products?limit=100");
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  // fetchProducts();
   return (
     <>
       <p className="bg-red-200">
